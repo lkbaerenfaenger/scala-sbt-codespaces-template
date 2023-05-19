89c064715737b7aa80bf17d33b5c6b1db7cc5f66
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2023-05-19 12:16:29 +0000

    Add custom bash prompt

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,7 @@
+# post-create.sh
+# This script is executed after the container is created.
+
+# Create custom bash prompt
+echo "" >> ~/.bashrc
+echo "# Custom bash prompt" >> ~/.bashrc
+echo "PS1='\e[0;32m\u\e[0m → \e[0;34m\W\e[0m \$ '" >> ~/.bashrc
